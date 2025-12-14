7ffe7acfdbf02cfefa225a0ece606defc185b08d
unknown <sadmanrahman438@gmail.com>
2025-12-14 12:48:09 +0600

    Main File added

---
+++ main_file.php
@@ -0,0 +1,8 @@
+function calculateInvoiceTotal(array $products, float &taxrate = 0, float $couponAmount = 0):float
+{
+    return 0;
+}
+$products = [
+    ['UnitePrice' => 200, 'quantity => 2'],    
+    ['UnitePrice' => 150, 'quantity => 3'], 
+];
